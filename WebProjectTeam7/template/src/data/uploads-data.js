cc9c958995b83868bd2a852b1f62dd17b398cff4
Stefan-ys <stefanstoyanov001@gmail.com>
2024-07-11 16:00:02 +0300

    verify input at addUpload

--- WebProjectTeam7/template/src/data/uploads-data.js
+++ WebProjectTeam7/template/src/data/uploads-data.js
@@ -1,8 +1,10 @@
 let uploads = JSON.parse(localStorage.getItem('uploads')) || [];
 
 export const addUpload = (gifId) => {
-    uploads.push(gifId);
-    localStorage.setItem('uploads', JSON.stringify(uploads));
+    if (gifId) {
+        uploads.push(gifId);
+        localStorage.setItem('uploads', JSON.stringify(uploads));
+    }
 };
 
 export const getUploads = () => [...uploads];

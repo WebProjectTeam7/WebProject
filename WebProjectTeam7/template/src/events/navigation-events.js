28cafd1d243994aa99aef0858f7cfef73878ca75
NedaMarinowa <lilyana.palikova1311@gmail.com>
2024-07-11 14:05:35 +0300

    renderGiftsDetails- update, import loadTrending in navigation-events

--- WebProjectTeam7/template/src/events/navigation-events.js
+++ WebProjectTeam7/template/src/events/navigation-events.js
@@ -3,7 +3,7 @@ import { q, setActiveNav } from './helpers.js';
 import { toUploadView } from '../view/upload-view.js';
 import { toMyUploadsView } from '../view/my-uploads-view.js';
 import { getUploads } from '../data/uploads-data.js';
-import { loadSingleGif } from '../requests/request-service.js';
+import { loadSingleGif, loadTrending  } from '../requests/request-service.js';
 import { getFavorites } from '../data/favorites-data.js';
 
 // public API
@@ -50,7 +50,7 @@ const renderHome = () => {
     // q(CONTAINER_SELECTOR).innerHTML = toHomeView();
 };
 
-const renderTrending = async () => { 
+export const renderTrending = async () => { 
     const trending = await loadTrending();
 
     q(CONTAINER_SELECTOR).innerHTML = toTrendingView(trending)
@@ -80,7 +80,5 @@ const renderAbout = () => {
 };
 
 export const renderGiftsDetails = async (categoryId = null) => {
-    const gif = await loadDisplayGifDetails();
-
-    q(CONTAINER_SELECTOR).innerHTML = toSingleGifView(gif)
+    q(CONTAINER_SELECTOR).innerHTML = toSingleGifView()
 }

0a4138c5b81353d154bd76ca65227ccae00a9939
Stefan-ys <stefanstoyanov001@gmail.com>
2024-07-12 14:55:19 +0300

    add changes to css for home page view

--- WebProjectTeam7/template/src/view/home-view.js
+++ WebProjectTeam7/template/src/view/home-view.js
@@ -11,10 +11,13 @@ export const toHomeView = (gifs) => `
       <li>Add and remove GIFs from favorites</li>
       <li>Search for GIFs</li>
     </ul>
-    <img src="https://api.giphy.com/v1/gifs/random" class="gif top-left" alt="GIF 1">
-    <img src="https://api.giphy.com/v1/gifs/random" class="gif top-right" alt="GIF 2">
-    <img src="https://api.giphy.com/v1/gifs/random" class="gif bottom-left-1" alt="GIF 3">
-    <img src="https://api.giphy.com/v1/gifs/random" class="gif bottom-left-2" alt="GIF 4">
+    <img src="images/gifs/loading.gif" class="gif top-left" alt="GIF 1">
+    <img src="images/gifs/loading.gif" class="gif top-right" alt="GIF 2">
+    <img src="images/gifs/loading.gif" class="gif bottom-left-1" alt="GIF 3">
+    <img src="images/gifs/loading.gif" class="gif bottom-left-2" alt="GIF 4">
+    <img src="images/gifs/loading.gif" class="gif bottom-right-1" alt="GIF 5">
+    <img src="images/gifs/loading.gif" class="gif bottom-right-2" alt="GIF 6">
+    <img src="images/gifs/loading.gif" class="gif bottom-right-3" alt="GIF 7">
   </div>
 </div>
 </div>

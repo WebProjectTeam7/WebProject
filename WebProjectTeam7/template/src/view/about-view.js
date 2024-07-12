f6caaad3231eafa00d6589511f8190c854ba5ed5
YordanovDanail <166701329+YordanovDanail@users.noreply.github.com>
2024-07-12 15:01:26 +0300

    small  change

--- WebProjectTeam7/template/src/view/about-view.js
+++ WebProjectTeam7/template/src/view/about-view.js
@@ -3,7 +3,10 @@ export const toAboutView = () => `
   <div class="content">
     <h1>About the app</h1>
     <h2>Authors: Team 7</h2>
-    <h2>Date: 17.07.2024</h2>
+    <h2>Stefan Stoyanov</h2>
+    <h2>Lilyana Palikova</h2>
+    <h2>Danail Yordanov</h2>
+    <h4>Date: 17.07.2024</h2>
   </div>
 </div>
 `;

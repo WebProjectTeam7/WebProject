5bc3defd4a575b494682fd55a676b8bb4af34bb4
YordanovDanail <166701329+YordanovDanail@users.noreply.github.com>
2024-07-13 15:22:59 +0300

    about style

--- WebProjectTeam7/template/src/view/about-view.js
+++ WebProjectTeam7/template/src/view/about-view.js
@@ -2,11 +2,11 @@ export const toAboutView = () => `
 <div id="about">
   <div class="content">
     <h1>About the app</h1>
-    <h2>Authors: Team 7</h2>
+    <img src="./images/team7-logo.jpg" alt="Team 7 Logo">
     <h2>Stefan Stoyanov</h2>
     <h2>Lilyana Palikova</h2>
     <h2>Danail Yordanov</h2>
-    <h4>Date: 17.07.2024</h2>
+    <h4>Date: 17.07.2024</h4>
   </div>
 </div>
 `;

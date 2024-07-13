552a8b5fad4280c9382dcc35aba20dd33233a824
Stefan-ys <stefanstoyanov001@gmail.com>
2024-07-13 18:55:10 +0300

    add styles for upload menu

--- WebProjectTeam7/template/src/view/upload-view.js
+++ WebProjectTeam7/template/src/view/upload-view.js
@@ -1,8 +1,10 @@
 export const toUploadView = () => `
+<div class="upload">
   <h2>Upload a GIF</h2>
   <form class="upload-form">
       <input type="file" id="gif-file" accept="image/gif">
       <!-- <input type="text" id="gif-url" placeholder="Or enter a GIF URL"> -->
       <button class="upload-btn" type="submit">Upload GIF</button>
   </form>
+</div>
 `;

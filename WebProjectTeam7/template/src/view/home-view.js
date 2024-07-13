9696434a4f9a105bcf00e6c7a341c5a6302ed46f
Stefan-ys <stefanstoyanov001@gmail.com>
2024-07-13 19:16:35 +0300

    add homepage styles and fixes

--- WebProjectTeam7/template/src/view/home-view.js
+++ WebProjectTeam7/template/src/view/home-view.js
@@ -1,16 +1,11 @@
 export const toHomeView = (gifs) => `
 <div id="home" class="home-view">
   <h1>GIPHY GIFS</h1>
-  <div class="content">
-    <p>Powered by: </p>
+  <div class="powered-by">
+    <p>Powered by:</p>
     <img src="images/logos/gyphy-logo.png" alt="Giphy">
-    <ul>
-      <li>Browse trending</li>
-      <li>Upload GIF</li>
-      <li>Browse GIFs</li>
-      <li>Add and remove GIFs from favorites</li>
-      <li>Search for GIFs</li>
-    </ul>
+  </div>
+  <div class="content">
     <img src="${gifs ? gifs[0].images.downsized.url : 'images/gifs/loading.gif'}" class="gif top-left" alt="GIF 1">
     <img src="${gifs ? gifs[1].images.downsized.url : 'images/gifs/loading.gif'}" class="gif top-right" alt="GIF 2">
     <img src="${gifs ? gifs[2].images.downsized.url : 'images/gifs/loading.gif'}" class="gif bottom-left-1" alt="GIF 3">
@@ -18,7 +13,13 @@ export const toHomeView = (gifs) => `
     <img src="${gifs ? gifs[4].images.downsized.url : 'images/gifs/loading.gif'}" class="gif bottom-right-1" alt="GIF 5">
     <img src="${gifs ? gifs[5].images.downsized.url : 'images/gifs/loading.gif'}" class="gif bottom-right-2" alt="GIF 6">
     <img src="${gifs ? gifs[6].images.downsized.url : 'images/gifs/loading.gif'}" class="gif bottom-right-3" alt="GIF 7">
+    <ul>
+      <li>Browse trending</li>
+      <li>Upload GIF</li>
+      <li>Browse GIFs</li>
+      <li>Keep you favorite GIF</li>
+      <li>Search for GIFs</li>
+    </ul>
   </div>
-</div>
 </div>
 `;

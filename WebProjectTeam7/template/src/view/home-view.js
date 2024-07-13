46a85bc92251a8f5ac4587ba705a4bb9f080df43
Stefan-ys <stefanstoyanov001@gmail.com>
2024-07-13 09:38:48 +0300

    add default gif for home page view when error

--- WebProjectTeam7/template/src/view/home-view.js
+++ WebProjectTeam7/template/src/view/home-view.js
@@ -11,13 +11,13 @@ export const toHomeView = (gifs) => `
       <li>Add and remove GIFs from favorites</li>
       <li>Search for GIFs</li>
     </ul>
-    <img src="${gifs[0].images.downsized.url}" class="gif top-left" alt="GIF 1">
-    <img src="${gifs[1].images.downsized.url}" class="gif top-right" alt="GIF 2">
-    <img src="${gifs[2].images.downsized.url}" class="gif bottom-left-1" alt="GIF 3">
-    <img src="${gifs[3].images.downsized.url}" class="gif bottom-left-2" alt="GIF 4">
-    <img src="${gifs[4].images.downsized.url}" class="gif bottom-right-1" alt="GIF 5">
-    <img src="${gifs[5].images.downsized.url}" class="gif bottom-right-2" alt="GIF 6">
-    <img src="${gifs[6].images.downsized.url}" class="gif bottom-right-3" alt="GIF 7">
+    <img src="${gifs ? gifs[0].images.downsized.url : 'images/gifs/loading.gif'}" class="gif top-left" alt="GIF 1">
+    <img src="${gifs ? gifs[1].images.downsized.url : 'images/gifs/loading.gif'}" class="gif top-right" alt="GIF 2">
+    <img src="${gifs ? gifs[2].images.downsized.url : 'images/gifs/loading.gif'}" class="gif bottom-left-1" alt="GIF 3">
+    <img src="${gifs ? gifs[3].images.downsized.url : 'images/gifs/loading.gif'}" class="gif bottom-left-2" alt="GIF 4">
+    <img src="${gifs ? gifs[4].images.downsized.url : 'images/gifs/loading.gif'}" class="gif bottom-right-1" alt="GIF 5">
+    <img src="${gifs ? gifs[5].images.downsized.url : 'images/gifs/loading.gif'}" class="gif bottom-right-2" alt="GIF 6">
+    <img src="${gifs ? gifs[6].images.downsized.url : 'images/gifs/loading.gif'}" class="gif bottom-right-3" alt="GIF 7">
   </div>
 </div>
 </div>

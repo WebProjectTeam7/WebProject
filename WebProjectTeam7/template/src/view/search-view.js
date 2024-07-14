348709732c0d9e9c3227b7fa2cacd1abdde64d81
YordanovDanail <166701329+YordanovDanail@users.noreply.github.com>
2024-07-14 19:31:37 +0300

    search-view documentation

--- WebProjectTeam7/template/src/view/search-view.js
+++ WebProjectTeam7/template/src/view/search-view.js
@@ -1,5 +1,11 @@
 import { toGifSimple } from './gifs-view.js';
 
+/**
+ * Generates the HTML for displaying search results.
+ * @param {Array} giphs - Array of GIF objects matching the search term.
+ * @param {string} searchTerm - The search term used to find the GIFs.
+ * @returns {string} - HTML string for displaying search results.
+ */
 export const toSearchView = (giphs, searchTerm) => `
 <div id="giphs">
   <h1>Giphs found for "${searchTerm}":</h1>

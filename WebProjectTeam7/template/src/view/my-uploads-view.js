2da0f6c58aec680dfb55d1faf4e593c3d6d27b19
YordanovDanail <166701329+YordanovDanail@users.noreply.github.com>
2024-07-14 19:30:23 +0300

    my-upload-view documentation

--- WebProjectTeam7/template/src/view/my-uploads-view.js
+++ WebProjectTeam7/template/src/view/my-uploads-view.js
@@ -1,5 +1,10 @@
 import { toGifSimple } from './gifs-view.js';
 
+/**
+ * Generates the HTML for displaying the user's uploaded GIFs.
+ * @param {Array} gifs - Array of GIF objects uploaded by the user.
+ * @returns {string} - HTML string for displaying the user's uploaded GIFs.
+ */
 export const toMyUploadsView = (gifs) => `
 <div id="gifs">
   <h1>My Uploaded GIFs:</h1>
